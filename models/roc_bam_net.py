0d4f799e147a5b3033b091972f190d35b8795138
liticia-wq <liticiahamiche@gmail.com>
2026-09-13 14:08:53 +0200

    Implement RoCBAM_Net with attention mechanisms

--- models/roc_bam_net.py
+++ models/roc_bam_net.py
@@ -1,1 +1,109 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_cat = torch.cat([avg_out, max_out], dim=1)
+        return x * self.sigmoid(self.conv(x_cat))
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        return x * self.sigmoid(avg_out + max_out)
+
+class CBAM(nn.Module):
+    def __init__(self, channels):
+        super(CBAM, self).__init__()
+        self.ca = ChannelAttention(channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        return self.sa(self.ca(x))
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class RoCBAMNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):
+        super(RoCBAMNet, self).__init__()
+        self.e1 = ConvBlock(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        
+        self.e2 = ConvBlock(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        
+        self.e3 = ConvBlock(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        
+        self.b = ConvBlock(256, 512)
+        self.cbam = CBAM(512)
+
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.d3 = ConvBlock(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.d2 = ConvBlock(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.d1 = ConvBlock(128, 64)
+
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        e1 = self.e1(x)
+        p1 = self.pool1(e1)
+
+        e2 = self.e2(p1)
+        p2 = self.pool2(e2)
+
+        e3 = self.e3(p2)
+        p3 = self.pool3(e3)
+
+        b = self.b(p3)
+        b = self.cbam(b)
+
+        d3 = self.up3(b)
+        d3 = torch.cat([d3, e3], dim=1)
+        d3 = self.d3(d3)
+
+        d2 = self.up2(d3)
+        d2 = torch.cat([d2, e2], dim=1)
+        d2 = self.d2(d2)
+
+        d1 = self.up1(d2)
+        d1 = torch.cat([d1, e1], dim=1)
+        d1 = self.d1(d1)
+
+        return self.out(d1)
