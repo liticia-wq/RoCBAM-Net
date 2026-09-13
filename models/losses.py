4b2749e99f44807ba7f8149b76b9d5f7b1b7e180
liticia-wq <liticiahamiche@gmail.com>
2026-09-13 14:13:20 +0200

    Implement WeightedBCEDiceLoss class

--- models/losses.py
+++ models/losses.py
@@ -1,1 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class WeightedBCEDiceLoss(nn.Module):
+    def __init__(self, weight=50.0):
+        super(WeightedBCEDiceLoss, self).__init__()
+        self.weight = weight
+
+    def forward(self, inputs, targets):
+        bce = F.binary_cross_entropy_with_logits(inputs, targets)
+        pred = torch.sigmoid(inputs)
+        smooth = 1e-5
+        intersection = (pred * targets).sum()
+        dice = (2. * intersection + smooth) / (pred.sum() + targets.sum() + smooth)
+        dice_loss = 1 - dice
+        return bce + (self.weight * dice_loss)
+
+def get_dice(inputs, targets):
+    pred = torch.sigmoid(inputs)
+    pred = (pred > 0.5).float()
+    smooth = 1e-5
+    intersection = (pred * targets).sum()
+    return (2. * intersection + smooth) / (pred.sum() + targets.sum() + smooth)
